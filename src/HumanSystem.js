63822286dcfb92918f79192ce63cfef155c21c9c
machineblade <machine.blade2991@gmail.com>
2026-05-25 10:22:11 -0400

    breathing

---
+++ src/HumanSystem.js
@@ -0,0 +1,23 @@
+import * as PIXI from "pixi.js";
+
+export default class Human extends PIXI.Container {
+  constructor(name) {
+    super();
+
+    this.name = name;
+    this.breathingInterval = null;
+  };
+  breatheIn() {
+
+  }
+  breatheOut() {
+
+  }
+  breathe() {
+    this.breatheIn();
+    setTimeout(this.breatheOut, 500);
+  }
+  startBreathing() {
+    this.breathingInterval = setInterval(this.breathe(), 3000);
+  }
+}
